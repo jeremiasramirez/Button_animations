8e6f82a912848f94ddd678555e5f7205fc2b2661
jeremias ramirez <jereramirez499@gmail.com>
2019-04-18 23:33:02 -0400

    agregando interaccion y mas buttons

---
+++ public/js/index.js
@@ -0,0 +1,8 @@
+// let buttonClicks = document.getElementById("button__hover__click--js");
+
+// let animationsButton = button => {
+// 	button.addEventListener("click", (e)=>{ 
+// 	 	button.classList.toggle("scale")
+// 	}, false);
+// }
+// animationsButton(buttonClicks);
